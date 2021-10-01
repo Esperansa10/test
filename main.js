fb41fe1886689f5fc103fd29c817d01aceefc0e1
Esperansa10 <81090237+Esperansa10@users.noreply.github.com>
2021-10-01 16:05:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//var audio = $("#music"); не работает
+//var audio = document.getElementById('music');
+//audio.innerHTML ='Hi';
+//var text = $("#information").html();
+
+/* var tags = $(".block");
+tags.css("font-size", "15px");
+
+function fun() {
+  if ($('textarea').val() == "спрятать")
+  return $('.block:eq(1)').html('Та да');
+  else {
+    return $('.block:eq(1)').html('Та no');
+
+  }
+};
+*/
+
+function changeColor(ad) {
+	$("#button").attr ("class", ad);
+}
